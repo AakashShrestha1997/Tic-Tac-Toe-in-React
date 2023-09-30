3d760d0fd8bcbd94b624ed26e63fcacf9bd05c30
AakashShrestha 1997 <aakashdarksky@gmail.com>
2023-09-30 12:48:56 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+import "./App.css";
+import TicTacToe from "./Components/TicTacToe";
+
+function App() {
+  // const [resource, setResource] = useState("Post");
+  return (
+    <>
+      <div>
+        <TicTacToe />
+      </div>
+    </>
+  );
+}
+
+export default App;
